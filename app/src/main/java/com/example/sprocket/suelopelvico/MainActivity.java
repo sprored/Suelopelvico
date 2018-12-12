406275927b1d65506c170efddffbf5a88fa725b1
sprored <josea.mc@gmail.com>
2018-12-12 17:38:03 +0100

    Add code for SoundPool

--- app/src/main/java/com/example/sprocket/suelopelvico/MainActivity.java
+++ app/src/main/java/com/example/sprocket/suelopelvico/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.sprocket.suelopelvico;
 
+import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.media.SoundPool;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -12,11 +14,12 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     final int RELAXATION = 0;
-    final int RELAXATION_SECONDS = 12;
+    final int RELAXATION_SECONDS = 6;
     final int CONTRACTION = 1;
     final int CONTRACTION_SECONDS = 6;
     final int TOTAL_MINUTES = 10;
     final int TOTAL_TURNS = (TOTAL_MINUTES * 60) / (CONTRACTION_SECONDS + RELAXATION_SECONDS);
+    SoundPool soundPool;
 
     Handler handler = new Handler();
     int state = 0;
@@ -28,20 +31,44 @@ public class MainActivity extends AppCompatActivity {
             // Change screen
             TextView textViewMain = (TextView) findViewById(R.id.main_display);
             TextView textViewSecondary = (TextView) findViewById(R.id.secondary_display);
+
+            /* En el constructor de SoundPool hay que indicar tres parámetros.
+            El primero corresponde al máximo de reproducciones simultáneas.
+            El segundo es el tipo de stream de audio (normalmente STREAM_MUSIC).
+            El tercero es la calidad de reproducción, aunque actualmente no se implementa.
+             */
+            int idContract, idRelax, idFinal;
+            soundPool = new SoundPool(2,AudioManager.STREAM_MUSIC,0);
+            idContract = soundPool.load(getApplicationContext(),R.raw.contract,0);
+            idRelax = soundPool.load(getApplicationContext(),R.raw.relax,0);
+            idFinal = soundPool.load(getApplicationContext(),R.raw.final_sound,0);
+
             MediaPlayer mpContraction = MediaPlayer.create(getApplicationContext(), R.raw.contract);
             MediaPlayer mpRelaxation = MediaPlayer.create(getApplicationContext(), R.raw.relax);
-            MediaPlayer mpFinal = MediaPlayer.create(getApplicationContext(), R.raw.final_sound);
 
             if ((state == RELAXATION) && (counter < TOTAL_TURNS)) {
-                mpContraction.start();
+                /*El método play() permite reproducir una pista. Hay que indicarle
+                el identificador de pista;
+                el volumen para el canal izquierdo y derecho (0.0 a 1.0);
+                La prioridad;
+                El número de repeticiones (-1= siempre, 0=solo una vez, 1=repetir una vez, …  )
+                y el ratio de reproducción, con el que podremos modificar la velocidad o pitch
+                (1.0 reproducción normal, rango: 0.5 a 2.0)
+
+                 */
+                soundPool.play(idContract,1,1,1,0,1);
                 state = CONTRACTION;
                 counter = counter + 1;
                 textViewMain.setText(R.string.contraction_message);
                 textViewMain.setBackgroundColor(getResources().getColor(R.color.contractionColor));
                 textViewSecondary.setText((String) getText(R.string.secondary_message) + " " + counter + " / " + TOTAL_TURNS);
-                handler.postDelayed(runnableCode, CONTRACTION_SECONDS * 1000);
+                handler.postDelayed(runnableCode, CONTRACTION_SECONDS * 1100);
 
             } else if ((state == CONTRACTION) && (counter < TOTAL_TURNS)) {
+                if (mpRelaxation.isPlaying()) {
+                    mpRelaxation.release();
+                    mpRelaxation = MediaPlayer.create(getApplicationContext(), R.raw.relax);
+                }
                 mpRelaxation.start();
                 state = RELAXATION;
                 textViewMain.setText(R.string.relaxation_message);
@@ -49,14 +76,12 @@ public class MainActivity extends AppCompatActivity {
                 handler.postDelayed(runnableCode, RELAXATION_SECONDS * 1000);
 
             } else {
+                MediaPlayer mpFinal = MediaPlayer.create(getApplicationContext(), R.raw.final_sound);
                 mpFinal.start();
                 textViewMain.setText(R.string.end_message);
                 textViewMain.setBackgroundColor(getResources().getColor(R.color.introColor));
                 mpContraction.release();
-                mpContraction = null;
                 mpRelaxation.release();
-                mpRelaxation = null;
-                mpFinal = null;
             }
 
         }
