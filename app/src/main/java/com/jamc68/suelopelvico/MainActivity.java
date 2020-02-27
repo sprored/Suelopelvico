3766425e33ff1e203de2e9b687ed36213c717353
sprored <josea.mc@gmail.com>
2020-02-27 23:01:38 +0100

    refactor: remove ExerciseData class

--- app/src/main/java/com/jamc68/suelopelvico/MainActivity.java
+++ app/src/main/java/com/jamc68/suelopelvico/MainActivity.java
@@ -1,8 +1,10 @@
 package com.jamc68.suelopelvico;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Bundle;
@@ -15,10 +17,20 @@ import android.view.WindowManager;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static int totalMinutes = 10;
+    private static final int RELAXATION_SECONDS = 12;
+    private static final int CONTRACTION_SECONDS = 6;
 
-    // Fields
+    private void setupSharedPreferences() {
+        // Get all of the values from shared preferences to set it up
+        SharedPreferences setupSharedPreferences =
+                PreferenceManager.getDefaultSharedPreferences(this);
+    }
+
+    // Fields for runnable code
     private static int turnCounter = 0;
     private static final boolean isRelaxed = true;
+    private static final int TOTAL_TURNS = (totalMinutes * 60) / (CONTRACTION_SECONDS + RELAXATION_SECONDS);
     Handler handler = new Handler();
 
     // Start runnableCode
@@ -32,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
             TextView textViewMain = (TextView) findViewById(R.id.main_display);
             TextView textViewSecondary = (TextView) findViewById(R.id.secondary_display);
 
-            if ((state == isRelaxed) && (turnCounter < ExerciseData.getTOTAL_TURNS())) {
+            if ((state == isRelaxed) && (turnCounter < TOTAL_TURNS)) {
                 mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.contract);
                 mediaPlayer.start();
                 mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
@@ -47,10 +59,10 @@ public class MainActivity extends AppCompatActivity {
                 textViewMain.setTextColor(getResources().getColor(R.color.secondaryTextColor));
                 textViewMain.setBackgroundColor(getResources().getColor(R.color.contractionColor));
                 textViewSecondary.setText(
-                        getString(R.string.secondary_message, turnCounter, ExerciseData.getTOTAL_TURNS()));
-                handler.postDelayed(runnableExerciseSession, ExerciseData.getCONTRACTION_SECONDS() * 1000);
+                        getString(R.string.secondary_message, turnCounter, TOTAL_TURNS));
+                handler.postDelayed(runnableExerciseSession, CONTRACTION_SECONDS * 1000);
 
-            } else if ((state == !isRelaxed) && (turnCounter < ExerciseData.getTOTAL_TURNS())) {
+            } else if ((state == !isRelaxed) && (turnCounter < TOTAL_TURNS)) {
                 mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.relax);
                 mediaPlayer.start();
                 mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
@@ -63,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
                 textViewMain.setText(R.string.relaxation_message);
                 textViewMain.setTextColor(getResources().getColor(R.color.primaryTextColor));
                 textViewMain.setBackgroundColor(getResources().getColor(R.color.relaxColor));
-                handler.postDelayed(runnableExerciseSession, ExerciseData.getRELAXATION_SECONDS() * 1000);
+                handler.postDelayed(runnableExerciseSession, RELAXATION_SECONDS * 1000);
 
             } else {
                 mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.final_sound);
@@ -130,8 +142,7 @@ public class MainActivity extends AppCompatActivity {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
         TextView textViewSecondary = (TextView) findViewById(R.id.secondary_display);
-        textViewSecondary.setText(ExerciseData.getTOTAL_TURNS() + " " + getText(R.string.secondary_message));
-        textViewSecondary.setText(getString(R.string.secundary_intro_message,ExerciseData.getTotalMinutes() ));
+        textViewSecondary.setText(getString(R.string.secundary_intro_message,totalMinutes ));
     }
 }
 
